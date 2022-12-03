5d9dbae1435f11d19f8f63d7345add3a41d0babb
Xurape <xarope1337@gmail.com>
2022-12-03 19:13:32 +0000

    Added 2 fonts and removed font-sans from body

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,9 @@
 
         <!-- Fonts -->
         {{-- <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap"> --}}
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Assistant&family=Ubuntu:wght@300;400&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @routes
@@ -17,7 +20,7 @@
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="antialiased">
         @inertia
     </body>
 </html>
